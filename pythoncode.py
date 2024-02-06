6674a864c605d3d290c59decedf795999c248b2d
tatephughes <etatephughes@gmail.com>
2024-02-06 11:33:09 +0000

    Fixed python tangle

--- pythoncode.py
+++ pythoncode.py
@@ -17,5 +17,3 @@ print(result)
 def my_or_function(bool1,bool2): return bool1 or bool2
 
 print(my_or_function(bool_func_1(),bool_func_2()))
-
-LazyList.iterate(x0)((x:DenseVector[Double]) => one_MRTH_step(x,q,r)
